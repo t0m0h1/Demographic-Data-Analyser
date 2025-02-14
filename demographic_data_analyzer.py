3e8763b458bed5c7e96510cf60175071c6bd8197
t0m0h1 <thomashathawayhogg@gmail.com>
2025-02-14 00:34:35 +0000

    minimum work hours calculation

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -30,10 +30,10 @@ def calculate_demographic_data(print_data=True):
     lower_education_rich = lower_education_rich.mean() * 100
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df['hours-per-week'] == min_work_hours]
 
     rich_percentage = None
 
